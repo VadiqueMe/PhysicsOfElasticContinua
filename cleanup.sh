bc7072c7c9dfe23533a6a19633ea1c451ed3dc43
Vadique Myself <vadiquemyself@gmail.com>
2023-01-03 03:05:49 +0300

    make the book buildable again
    all is ok now but the section 4.3

--- cleanup.sh
+++ cleanup.sh
@@ -9,7 +9,7 @@ find . -iname "*.synctex*" -type f -print0 | xargs -0 rm -v 2>/dev/null
 
 rm -f LICENSE.aux
 
-rm -v -f mybook.*.pdf
+##rm -v -f mybook.*.pdf
 rm -v -f mybook.*.out
 rm -v -f mybook.*.toc
 rm -v -f mybook.*.aux
---
+++ makechapter6.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+pdflatex mybook.thermoelasticity.tex
